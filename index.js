bff015fd91ac0887a43fcdd55510f24b3775bf85
Jahsonte Evans <jahsonteevans@gmail.com>
2022-10-19 18:07:31 -0600

    commit tree 7ced8f070772305fbd2b3a905bf428ce1723d432

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution in this file!
+const employee = {
+    name:'Sonte',
+    streetAddress: '94 Legend Drive'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    // console.log("newPeeps")
+    // console.log (employee)
+    // console.log(key)
+    // console.log(value)
+    // console.log(newObj)
+    const newObj = { ...employee};
+        newObj[key] = value;
+        return newObj
+    }
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {...employee}
+    console.log("Before",employee)
+    delete newEmployee['name']
+    console.log("After",employee)
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    
+    delete employee[key]
+    return employee
+}
+
+
+
+
